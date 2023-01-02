e44ca80361765622688290cc113cc6c0e1bf31d8
sharpSteff <sharp.kneidl@gmail.com>
2023-01-02 01:07:43 +0100

    fix startup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-// Deny usage of print and eprint as it won't have same result
-// in WASI as if doing in standard program, you must really know
-// what you are doing to disable that lint (and you don't know)
-#![deny(clippy::print_stdout)]
-#![deny(clippy::print_stderr)]
-
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use lapce_plugin::{
     psp_types::{
-        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url, MessageType},
+        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url},
         Request,
     },
     register_plugin, LapcePlugin, VoltEnvironment, PLUGIN_RPC,
@@ -19,26 +13,31 @@ struct State {}
 
 register_plugin!(State);
 
+macro_rules! string {
+    ( $x:expr ) => {
+        String::from($x)
+    };
+}
+
+macro_rules! ok {
+    ( $x:expr ) => {
+        match ($x) {
+            Ok(v) => v,
+            Err(e) => return Err(anyhow!(e)),
+        }
+    };
+}
 fn initialize(params: InitializeParams) -> Result<()> {
     let document_selector: DocumentSelector = vec![DocumentFilter {
-        // lsp language id
-        language: Some(String::from("csharp")),
-        // glob pattern
-        pattern: Some(String::from("**/*.{cs,csx}")),
-        // like file:
+        language: Some(string!("csharp")),
+        pattern: Some(string!("**/*.cs")),
         scheme: None,
     }];
-    // let mut server_args = vec![];
+    let mut server_args = vec![string!("--languageserver")];
 
-    // Check for user specified LSP server path
-    // ```
-    // [lapce-plugin-csharp.lsp]
-    // serverPath = "[path or filename]"
-    let mut server_args = vec![String::from("--languageserver")];
-    // ```
     if let Some(options) = params.initialization_options.as_ref() {
-        if let Some(lsp) = options.get("lsp") {
-            if let Some(args) = lsp.get("serverArgs") {
+        if let Some(volt) = options.get("volt") {
+            if let Some(args) = volt.get("serverArgs") {
                 if let Some(args) = args.as_array() {
                     if !args.is_empty() {
                         server_args = vec![];
@@ -51,10 +50,11 @@ fn initialize(params: InitializeParams) -> Result<()> {
                 }
             }
 
-            if let Some(server_path) = lsp.get("serverPath") {
+            if let Some(server_path) = volt.get("serverPath") {
                 if let Some(server_path) = server_path.as_str() {
                     if !server_path.is_empty() {
-                        let server_uri = Url::parse(&format!("urn:{}", server_path))?;
+                        print!("Starting LSP on {server_path}");
+                        let server_uri = ok!(Url::parse(&format!("urn:{}", server_path)));
                         PLUGIN_RPC.start_lsp(
                             server_uri,
                             server_args,
@@ -68,23 +68,16 @@ fn initialize(params: InitializeParams) -> Result<()> {
         }
     }
 
-    let file_name = "OmniSharp";
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("windows") => {
-            format!("{}.exe", file_name)
-        }
-        _ => file_name.to_owned(),
+    let file_name = match VoltEnvironment::operating_system().as_deref() {
+        Ok("windows") => "OmniSharp.exe",
+        _ => "OmniSharp",
     };
 
+    print!("Starting LSP on {file_name}");
+
     // Plugin working directory
-    let volt_uri = VoltEnvironment::uri()?;
-    let server_uri = Url::parse(&volt_uri)?.join(file_name)?;
+    let server_uri = Url::parse(file_name)?;
 
-    // if you want to use server from PATH
-    // let server_uri = Url::parse(&format!("urn:{filename}"))?;
-
-    // Available language IDs
-    // https://github.com/lapce/lapce/blob/HEAD/lapce-proxy/src/buffer.rs#L173
     PLUGIN_RPC.start_lsp(
         server_uri,
         server_args,
@@ -102,7 +95,7 @@ impl LapcePlugin for State {
             Initialize::METHOD => {
                 let params: InitializeParams = serde_json::from_value(params).unwrap();
                 if let Err(e) = initialize(params) {
-                    PLUGIN_RPC.window_show_message(MessageType::ERROR, format!("plugin returned with error: {e}"))
+                    PLUGIN_RPC.stderr(&format!("plugin returned with error: {e}"))
                 }
             }
             _ => {}
